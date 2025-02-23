7eb2cd380aafaca3d74780bd356c74612568a4b5
781coder <781betatest@gmail.com>
2025-02-23 15:57:25 -0500

    update to CANdle subsys

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -1,67 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-/*
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdleConfiguration;
-import com.ctre.phoenix.led.CANdle.LEDStripType;
-import com.ctre.phoenix.led.CANdle.VBatOutputMode;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants.CANdleConstants;
-
-//import frc.robot.CharToByteArray;
-
-public class CANdleSubsystem extends SubsystemBase {
-    
-    private final CANdle candle;
-    private final int LEDcnt = 80;
-
-    public CANdleSubsystem() {
-            candle = new CANdle(1);
-            CANdleConfiguration configAll = new CANdleConfiguration();
-            configAll.statusLedOffWhenActive = false;
-            configAll.disableWhenLOS = false;
-            configAll.stripType = LEDStripType.RGB;
-            configAll.brightnessScalar = 0.5;
-            configAll.vBatOutputMode = VBatOutputMode.Modulated;
-            candle.configAllSettings(configAll, 100);
-            //candle.modulateVBatOutput(0.5);
-
-    }
-
-    public void setALLLedtoColour(int[] rgb){
-        candle.setLEDs(0, 0, 0, 0, 0, 80);
-        //candle.modulateVBatOutput(1);
-    }
-
-
-    @Override
-    public void periodic() {
-        candle.setLEDs(255,0,0, 0, 0, 80);
-
-    
-    }
-
-    //public Command CANdleBlue() {
-        //return Commands.runOnce(() -> );
-    //}
-
-
-
-}
-*/
-
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 import com.ctre.phoenix.led.*;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
@@ -74,27 +13,21 @@ import com.ctre.phoenix.led.TwinkleOffAnimation.TwinkleOffPercent;
 public class CANdleSubsystem extends SubsystemBase {
     private final CANdle m_candle = new CANdle(1, "rio");
     private final int LedCount = 100;
-    private Joystick joystick;
 
     private Animation m_toAnimate = null;
 ;
 
     public enum AnimationTypes {
-        ColorFlow,
-        Fire,
-        Larson,
-        Rainbow,
-        RgbFade,
-        SingleFade,
-        Strobe,
-        Twinkle,
-        TwinkleOff,
-        SetAll
+        Idle,
+        Enabled,
+        Intake,
+        Outtake,
+        HasPiece,
+        PieceOut
     }
     private AnimationTypes m_currentAnimation;
 
-    public CANdleSubsystem(Joystick joy) {
-        this.joystick = joy;
+    public CANdleSubsystem() {
         CANdleConfiguration configAll = new CANdleConfiguration();
         configAll.statusLedOffWhenActive = true;
         configAll.disableWhenLOS = false;
@@ -102,11 +35,12 @@ public class CANdleSubsystem extends SubsystemBase {
         configAll.brightnessScalar = 0.5;
         configAll.vBatOutputMode = VBatOutputMode.Modulated;
         m_candle.configAllSettings(configAll, 100);
-        changeAnimation(AnimationTypes.SingleFade);
+
+        changeAnimation(AnimationTypes.Idle);
     }
 
     public void setColors() {
-        //changeAnimation(AnimationTypes.Rainbow);
+        changeAnimation(AnimationTypes.Idle);
     }
 
     /* Wrappers so we can access the CANdle from the subsystem */
@@ -124,57 +58,37 @@ public class CANdleSubsystem extends SubsystemBase {
         
         switch(toChange)
         {
-            case ColorFlow:
-                m_toAnimate = new ColorFlowAnimation(128, 20, 70, 0, 0.7, LedCount, Direction.Forward);
+            case Intake:
+                m_candle.setLEDs(0,0,255);
                 break;
-            case Fire:
-                m_toAnimate = new FireAnimation(1, 0.7, LedCount, 1, 0.1);
+            case Outtake:
+                m_candle.setLEDs(255,0,0);
                 break;
-            case Larson:
-                m_toAnimate = new LarsonAnimation(0, 255, 46, 0, 1, LedCount, BounceMode.Front, 3);
+            case HasPiece:
+                m_toAnimate = new StrobeAnimation(0, 0, 255, 0, 25.0/ 256.0, LedCount);
                 break;
-            case Rainbow:
-                m_toAnimate = new RainbowAnimation(1, 0.1, LedCount);
+            case PieceOut:
+                m_toAnimate = new StrobeAnimation(255, 0, 0, 0, 25.0/ 256.0, LedCount);
                 break;
-            case RgbFade:
-                m_toAnimate = new RgbFadeAnimation(0.7, 0.4, LedCount);
+            case Idle:
+                m_toAnimate = new SingleFadeAnimation(0, 255, 0, 0, 0.5, LedCount);
                 break;
-            case SingleFade:
-                m_toAnimate = new SingleFadeAnimation(50, 200, 2, 0, 0.5, LedCount);
-                break;
-            case Strobe:
-                m_toAnimate = new StrobeAnimation(240, 10, 180, 0, 98.0 / 256.0, LedCount);
-                break;
-            case Twinkle:
-                m_toAnimate = new TwinkleAnimation(30, 70, 60, 0, 0.4, LedCount, TwinklePercent.Percent6);
-                break;
-            case TwinkleOff:
-                m_toAnimate = new TwinkleOffAnimation(70, 90, 175, 0, 0.8, LedCount, TwinkleOffPercent.Percent100);
-                break;
-            case SetAll:
-                m_toAnimate = null;
+            case Enabled:
+                m_candle.setLEDs(0,255,0);
                 break;
         }
-        System.out.println("Changed to " + m_currentAnimation.toString());
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-        if(m_toAnimate == null) {
-            m_candle.setLEDs(200,0,200);
-
-        } else {
+        if(m_toAnimate != null) {
             m_candle.animate(m_toAnimate);
         }
        
        m_candle.modulateVBatOutput(1);
     }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
-    }
 }
 
 
