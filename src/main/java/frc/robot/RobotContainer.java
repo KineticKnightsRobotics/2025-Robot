343b6c2a64e894c9a8791bab013bf27393a896ba
781coder <781betatest@gmail.com>
2025-03-16 12:44:49 -0400

    Re-added the newer AquireCoral

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -335,7 +335,14 @@ public class RobotContainer {
         NamedCommands.registerCommand("OptimizedScoreLeft", teleopCommand.scoreCoralAuto_Optimized(ElevatorConstants.Positions.L4, DriveConstants.searchingSpeed, search));
         NamedCommands.registerCommand("OptimizedScoreRight", teleopCommand.scoreCoralAuto_Optimized(ElevatorConstants.Positions.L4, -DriveConstants.searchingSpeed, search));
         NamedCommands.registerCommand("HomeElevator", elevatorSubsystem.homeElevator().until(()-> elevatorSubsystem.getElevatorPosition() < 10));
-        NamedCommands.registerCommand("AquireCoral", coralSubsystem.loadCoral());
+        
+        // Might not work rn
+        NamedCommands.registerCommand("AquireCoral", 
+            new ParallelDeadlineGroup(
+                coralSubsystem.loadCoral(),
+                elevatorSubsystem.intakeElevator()
+            )
+        );
         // Set the new goal of the elevator to a scoring position when the "canExtend" eventMarker is passed
         NamedCommands.registerCommand("Extend", elevatorSubsystem.setElevatorGoal(ElevatorConstants.Positions.L4));
     }
